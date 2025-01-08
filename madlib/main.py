76e08788a6bc66352ed4cb5ab51ed9fc5807f3f6
dipanshupandey <dipanshupandey4488@gmail.com>
2025-01-08 13:18:34 +0530

    Set up basic Madlib story generator structure

--- madlib/main.py
+++ madlib/main.py
@@ -1,11 +1,1 @@
 import requests
-
-def getRandomStories():
-    url="https://funtranslations.com/api/translate"
-    resonse=requests.get(url)
-    if resonse.status_code ==200:
-        data=resonse
-        print(data.json())
-
-
-getRandomStories()
